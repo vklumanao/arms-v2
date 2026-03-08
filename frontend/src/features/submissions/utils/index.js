95f1a77c1f1b16397bcd99d4ef113a562d697ddc
Vicryl Kez Lumanao <wariyuri3@gmail.com>
2026-03-09 00:42:39 +0800

    refactor: remove exports for deleted submissions utilities

--- frontend/src/features/submissions/utils/index.js
+++ frontend/src/features/submissions/utils/index.js
@@ -1,5 +1,2 @@
-export * from "./mySubmissionsUtils";
-export * from "./publicationsUtils";
 export * from "./submissionFormUtils";
 export * from "./researchProjectsHubUtils";
-export * from "./mySubmissionsViewUtils";
