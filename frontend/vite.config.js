1c8ac697030ceae66a407a96ea581b4227a8fd82
Vicryl Kez Lumanao <wariyuri3@gmail.com>
2026-03-09 20:20:10 +0800

    feat(vite): enable polling watch for dockerized frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,4 +13,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 300,
+    },
+  },
 })
