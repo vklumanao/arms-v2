819055bd71f6f331af6b2608bb28984af8cf67eb
Vicryl Kez Lumanao <wariyuri3@gmail.com>
2026-03-09 20:19:38 +0800

    refactor(admin): remove AdminAffiliatesPage export

--- frontend/src/features/admin/index.js
+++ frontend/src/features/admin/index.js
@@ -1,5 +1,4 @@
 export { default as AdminAffiliatesModulePage } from "./pages/AdminAffiliatesModulePage";
-export { default as AdminAffiliatesPage } from "./pages/AdminAffiliatesPage";
 export { default as AdminControlsPage } from "./pages/AdminControlsPage";
 export { default as AdminResearchCenterPage } from "./pages/AdminResearchCenterPage";
 export { default as AdminUsersPage } from "./pages/AdminUsersPage";
