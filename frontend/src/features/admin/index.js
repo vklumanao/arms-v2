ff5d5665073bdafe18d9bb8fda64c1c54c44a26a
Vicryl Kez Lumanao <wariyuri3@gmail.com>
2026-03-11 01:16:03 +0800

    feat(admin): export department page

--- frontend/src/features/admin/index.js
+++ frontend/src/features/admin/index.js
@@ -1,5 +1,6 @@
 export { default as AdminAffiliatesModulePage } from "./pages/AdminAffiliatesModulePage";
 export { default as AdminControlsPage } from "./pages/AdminControlsPage";
+export { default as AdminDepartmentPage } from "./pages/AdminDepartmentPage";
 export { default as AdminResearchCenterPage } from "./pages/AdminResearchCenterPage";
 export { default as AdminUsersPage } from "./pages/AdminUsersPage";
 export * from "./services";
