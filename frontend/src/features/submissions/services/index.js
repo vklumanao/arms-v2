2f3c1323054964fd7b4138f6cc70e433456ea00e
Vicryl Kez Lumanao <wariyuri3@gmail.com>
2026-03-11 15:36:54 +0800

    chore(submissions): export award recognition service

--- frontend/src/features/submissions/services/index.js
+++ frontend/src/features/submissions/services/index.js
@@ -1,4 +1,5 @@
 export * from "./affiliateProfileService";
+export * from "./awardRecognitionService";
 export * from "./movService";
 export * from "./mySubmissionsService";
 export * from "./submissionService";
