5e8a1f613e41cfe1f6d048d4d7bb496235f50f13
Vicryl Kez Lumanao <wariyuri3@gmail.com>
2026-03-11 15:36:54 +0800

    chore(submissions): export award submission page

--- frontend/src/features/submissions/index.js
+++ frontend/src/features/submissions/index.js
@@ -3,5 +3,6 @@ export { default as AwardsRecognitionPage } from "./pages/AwardsRecognitionPage"
 export { default as ResearchOutputsPage } from "./pages/ResearchOutputsPage";
 export { default as ResearchProjectsHubPage } from "./pages/ResearchProjectsHubPage";
 export { default as SubmitAffiliationPage } from "./pages/SubmitAffiliationPage";
+export { default as SubmitAwardRecognitionPage } from "./pages/SubmitAwardRecognitionPage";
 export * from "./services";
 export * from "./utils";
