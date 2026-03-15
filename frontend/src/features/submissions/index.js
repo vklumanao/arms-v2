f6dbf30e20fda2f110cde7ef28c5aed9a6466653
Vicryl Kez Lumanao <wariyuri3@gmail.com>
2026-03-15 19:12:51 +0800

    feat(submissions): export project detail page

--- frontend/src/features/submissions/index.js
+++ frontend/src/features/submissions/index.js
@@ -1,6 +1,7 @@
 export { default as AffiliateProfilePage } from "./pages/AffiliateProfilePage";
 export { default as AwardsRecognitionPage } from "./pages/AwardsRecognitionPage";
 export { default as ResearchOutputsPage } from "./pages/ResearchOutputsPage";
+export { default as ResearchProjectDetailPage } from "./pages/ResearchProjectDetailPage";
 export { default as ResearchProjectsHubPage } from "./pages/ResearchProjectsHubPage";
 export { default as SubmitAffiliationPage } from "./pages/SubmitAffiliationPage";
 export { default as SubmitAwardRecognitionPage } from "./pages/SubmitAwardRecognitionPage";
