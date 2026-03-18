a490d267c012c4cae8ba94567b371ae641e3e96e
Vicryl Kez Lumanao <wariyuri3@gmail.com>
2026-03-18 16:11:22 +0800

    feat: export public record detail

--- frontend/src/features/public-records/index.js
+++ frontend/src/features/public-records/index.js
@@ -1,3 +1,4 @@
 export { default as PublicRecordsPage } from "./pages/PublicRecordsPage";
+export { default as PublicRecordDetailPage } from "./pages/PublicRecordDetailPage";
 export * from "./services";
 export * from "./utils";
