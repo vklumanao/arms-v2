9cda2dbfe1bf5148b8d67cf5dc7aa9eda24a70a1
Vicryl Kez Lumanao <wariyuri3@gmail.com>
2026-03-20 01:32:14 +0800

    chore: export public research center page

--- frontend/src/pages/public-records/index.js
+++ frontend/src/pages/public-records/index.js
@@ -1,2 +1,3 @@
 export { default as PublicRecordsPage } from "./PublicRecordsPage";
 export { default as PublicRecordDetailPage } from "./PublicRecordDetailPage";
+export { default as PublicResearchCenterDetailPage } from "./PublicResearchCenterDetailPage";
