2e8abb2d7c5d01c7729cb8326c4c65bce6ba9673
Vicryl Kez Lumanao <wariyuri3@gmail.com>
2026-04-20 21:57:42 +0800

    refactor(admin): replace admin controls exports with access control module exports

--- frontend/src/pages/admin/index.js
+++ frontend/src/pages/admin/index.js
@@ -1,6 +1,6 @@
 export { default as AdminAffiliatesModulePage } from "./AdminAffiliatesModulePage";
 export { default as AdminAffiliateDetailPage } from "./AdminAffiliateDetailPage";
-export { default as AdminControlsPage } from "./AdminControlsPage";
+export { default as AdminAccessControlPage } from "./AdminAccessControlPage";
 export { default as AdminDepartmentDetailPage } from "./AdminDepartmentDetailPage";
 export { default as AdminDepartmentPage } from "./AdminDepartmentPage";
 export { default as AdminResearchCenterDetailPage } from "./AdminResearchCenterDetailPage";
