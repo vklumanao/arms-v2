4d4ed5e65af6f3a77fbd76d67b5f16af03fa1eee
Vicryl Kez Lumanao <wariyuri3@gmail.com>
2026-04-20 21:57:43 +0800

    refactor(admin-utils): remove deprecated admin utility exports

--- frontend/src/utils/admin/index.js
+++ frontend/src/utils/admin/index.js
@@ -1,6 +1,5 @@
 export * from "./adminControlsUtils";
 export * from "./csv";
-export * from "./logAdminActivity";
 export * from "./paginationUtils";
 export * from "./adminAffiliatesModuleUtils";
 export * from "./adminResearchCenterUtils";
